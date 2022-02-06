ceee3842fcba12c9e7cc746ea928950a5acbeaa6
eggonz <egoitz.gonz@gmail.com>
2022-02-06 18:20:02 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='deelemma',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DeeLeMMa',
+    author='eggonz',
+    license='GPL-3.0',
+)
